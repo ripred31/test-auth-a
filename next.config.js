9bfda80d722c51c45266f26726d4c91621049f6f
ripred31 <ripred31@gmail.com>
2023-08-10 14:51:13 -0400

    ffs I hope this works

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   serverActions: true,
-  // },
+  experimental: {
+    serverActions: true,
+  },
+  distDir: 'public', // Add this line to specify the output directory
 }
 
 module.exports = nextConfig
